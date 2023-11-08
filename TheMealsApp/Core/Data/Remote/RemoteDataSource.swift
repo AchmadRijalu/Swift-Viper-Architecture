0c8f53df73b30314801715a1e0e51148b78ae5d7
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2023-11-08 23:08:28 +0700

    changing URL Session into Alamofire

---
+++ TheMealsApp/Core/Data/Remote/RemoteDataSource.swift
@@ -0,0 +1,58 @@
+//
+//  RemoteRepository.swift
+//  MealsApps
+//
+//  Created by Gilang Ramadhan on 11/08/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+protocol RemoteDataSourceProtocol: class {
+    
+    func getCategories(result: @escaping (Result<[CategoryResponse], URLError>) -> Void)
+    
+}
+
+final class RemoteDataSource: NSObject {
+    
+    private override init() { }
+    
+    static let sharedInstance: RemoteDataSource =  RemoteDataSource()
+    
+}
+
+extension RemoteDataSource: RemoteDataSourceProtocol {
+    
+    func getCategories(
+        result: @escaping (Result<[CategoryResponse], URLError>) -> Void
+    ) {
+        guard let url = URL(string: Endpoints.Gets.categories.url) else { return }
+        
+        AF.request(url).validate().responseDecodable(of: CategoriesResponse.self){ response in switch response.result{
+        case .success(let value):
+            result(.success(value.categories))
+        case .failure:
+            result(.failure(.invalidResponse))
+        }
+            
+        }
+        //    let task = URLSession.shared.dataTask(with: url) { maybeData, maybeResponse, maybeError in
+        //      if maybeError != nil {
+        //        result(.failure(.addressUnreachable(url)))
+        //      } else if let data = maybeData, let response = maybeResponse as? HTTPURLResponse, response.statusCode == 200 {
+        //        let decoder = JSONDecoder()
+        //        do {
+        //          let categories = try decoder.decode(CategoriesResponse.self, from: data).categories
+        //          result(.success(categories))
+        //        } catch {
+        //          result(.failure(.invalidResponse))
+        //        }
+        //      }
+        //    }
+        //    task.resume()
+        
+    }
+    
+}
